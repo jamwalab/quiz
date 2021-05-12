d530ea59a5eda4caf9bbad5ee3f17369554eefdd
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-05-12 03:28:25 -0400

    Quiz logic WIP 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
 var myDisplay = document.querySelector("#displayBox");
 
+var questions = [
+    {
+        quest: "Capital of Italy",
+        option: ["Rome","Milan","Venice","Madrid"],
+        ans: "Rome"
+    },
+    {
+        quest: "Capital of France",
+        option: ["Rome","Milan","Paris","Madrid"],
+        ans: "Paris"
+    },
+    {
+        quest: "Capital of Canada",
+        option: ["Rome","Milan","Venice","Ottawa"],
+        ans: "Ottawa"
+    },
+    {
+        quest: "Capital of Spain",
+        option: ["Rome","Milan","Venice","Madrid"],
+        ans: "Madrid"
+    },
+    {
+        quest: "Capital of Germany",
+        option: ["Rome","Berlin","Venice","Madrid"],
+        ans: "Berlin"
+    }
+]
+
 var displayThis = function(dataEl) {
     console.log(dataEl);
+
     myDisplay.appendChild(dataEl);
 };
 
@@ -17,6 +46,7 @@ var intro = function(event) {
     instruction.innerHTML = "<p>Try to answer the following code-related questions within the time limit</p><p>Keep in mind that incorrect answer will penalize your score / time by ten seconds!</p>"
     
     var btnContainer = document.createElement("div");
+    btnContainer.className = "btnContainer";
     var buttonStarQuiz = document.createElement("button");
     buttonStarQuiz.setAttribute("type", "submit");
     buttonStarQuiz.className = "btn";
@@ -31,3 +61,19 @@ var intro = function(event) {
 };
 
 window.addEventListener("load", intro);
+
+var playQuiz = function(event) {
+
+    for (var i=0; i<questions.length; i++) {
+        var myContainer = document.createElement("div");
+        myContainer.className = "display";
+        var heading = document.createElement("h2");
+        heading.className = "h2Heading";
+        heading.textContent = questions[i].quest;
+
+        for (var j=0; j<questions[i].option.length; j++) {
+
+        }
+    }
+    
+}
